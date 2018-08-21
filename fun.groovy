a2000f2e86653b77f5d1cceae4f0555dd611e393
Jay Modi <jmodi@fhlbdm.com>
2018-08-20 20:58:55 -0500

    intersect on collections

--- fun.groovy
+++ fun.groovy
@@ -1,7 +1,10 @@
 println "This is git fun with groovy."
 
 def list = ["abc","def", "a"]
+def list2 = ["def","a","z"]
 assert list.size() == 3
 
 println list.sort()
 assert list == ["a", "abc","def"]
+
+assert list.intersect(list2).sort() == ["a", "def"]
