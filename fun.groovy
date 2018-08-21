e74a937272041604056d68823fa588ae39c4d445
Jay Modi <jmodi@fhlbdm.com>
2018-08-20 21:46:24 -0500

    string literals should be escaped if we want value to it's key in map

--- fun.groovy
+++ fun.groovy
@@ -26,3 +26,22 @@ assert map.get('JAY') == null
 map.each { entry ->
 	println "Name: $entry.key ==> Age: $entry.value"
 }
+
+// grouping of list to create maps
+assert [
+        [name: 'Clark', city: 'London'], [name: 'Sharma', city: 'London'],
+        [name: 'Maradona', city: 'LA'], [name: 'Zhang', city: 'HK'],
+        [name: 'Ali', city: 'HK'], [name: 'Liu', city: 'HK'],
+].groupBy { it.city } == [
+        London: [[name: 'Clark', city: 'London'],
+                 [name: 'Sharma', city: 'London']],
+        LA    : [[name: 'Maradona', city: 'LA']],
+        HK    : [[name: 'Zhang', city: 'HK'],
+                 [name: 'Ali', city: 'HK'],
+                 [name: 'Liu', city: 'HK']],
+]
+
+// map literals
+def a = 'Bob'
+def ages = [(a): 43]
+assert ages['Bob'] == 43
