083ea3da27ad95453d06cb03d6a171efdcf2c29f
Jay Modi <jmodi@fhlbdm.com>
2018-08-20 21:01:29 -0500

    maps with groovy

--- fun.groovy
+++ fun.groovy
@@ -1,5 +1,6 @@
 println "This is git fun with groovy."
 
+// lists
 def list = ["abc","def", "a"]
 def list2 = ["def","a","z"]
 assert list.size() == 3
@@ -8,3 +9,15 @@ println list.sort()
 assert list == ["a", "abc","def"]
 
 assert list.intersect(list2).sort() == ["a", "def"]
+
+// maps
+def map = [
+	Bob : 42,
+	Alice: 54,
+	Max : 33
+]
+
+
+map.each { entry ->
+	println "Name: $entry.key ==> Age: $entry.value"
+}
